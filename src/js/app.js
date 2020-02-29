16652c7869cf892b0b24303b0c718cd16ca04ead
Marcin Chlewicki <chlewicki@gmail.com>
2020-02-29 14:13:21 +0100

    refactor

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,4 @@
 
 import '../scss/style.css'
 
-window.addEventListener("DOMContentLoaded", () => {
-    const scene = document.getElementById('scene');
-    const parallaxInstance = new Parallax(scene);
-
-    // const sea = document.getElementById('sea');
-    // const parallaxInstanceTwo = new Parallax(sea, {
-    //     limitX: 0,
-    //     limitY: false
-    // });
-});
+window.addEventListener("DOMContentLoaded", new Parallax(document.getElementById('scene')))
